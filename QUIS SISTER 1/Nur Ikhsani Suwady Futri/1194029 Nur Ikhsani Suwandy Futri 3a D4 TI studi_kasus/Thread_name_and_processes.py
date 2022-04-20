adb6c7b985312f44601549f1df69f671cbd27173
nurikhsanisf <nurikhsanisf@gmail.com>
2022-04-20 13:28:36 +0700

    mengembalikan file

---
+++ QUIS SISTER 1/Nur Ikhsani Suwady Futri/1194029 Nur Ikhsani Suwandy Futri 3a D4 TI studi_kasus/Thread_name_and_processes.py
@@ -0,0 +1,38 @@
+#studi kasus tentang perlombaan cerdas cermat soal yang dibagikan
+from threading import Thread
+import time
+import os
+
+class Tes (Thread):
+   def __init__(self, name):
+      Thread.__init__(self)
+      self.name = name
+ 
+   def run(self):
+       print("Soal dibagikan kepada peserta {}".format(self.name)) #, " is {} \n".format(os.getpid()))
+
+def main():
+    from random import randint
+    # Thread Creation
+    thread1 = Tes("soal 1 ")
+    thread2 = Tes("soal 2 ")
+  
+    # Thread Running
+    thread1.start()
+    thread2.start()
+
+
+    # Thread joining
+    thread1.join()
+    thread2.join()
+
+    # End 
+    print("soal selesai dibagikan")
+
+
+if __name__ == "__main__":
+    main()
+
+    
+
+
---
+++ QUIS SISTER 1/Nur Ikhsani Suwady Futri/1194029 Nur Ikhsani Suwandy Futri 3a D4 TI studi_kasus/semaphore.py
@@ -0,0 +1,46 @@
+#studi kasus tentang pengocokan arisan
+import logging
+import threading
+import time
+import random
+
+LOG_FORMAT = '%(asctime)s %(threadName)-17s %(levelname)-8s %(message)s'
+logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
+
+
+semaphore = threading.Semaphore(0) #menginisialisasi semphore ke 0 maka dapat 
+#memperoleh event semphore agardapat mengsingkronkan dua atau lebih thread
+item = 0 #sumber daya
+
+
+def pemenangarisan(): #akam memperoleh data dari methode acquire jika perhitungannya 
+    #0 dan akan memblokir methode acquire namun ketika perthitungan smaphorenya 
+    # #lebih dari 0 maka nilai akan berkurang.
+    logging.info('pengocokan arisan dimulai peserta harap menunggu')
+    semaphore.acquire() # ketika nomorarisan melepas semaphore maka pemenanarisan 
+    #akan memperoleh dan melakukan run pada bagian ini
+    logging.info('pemenang arisan dengan nomor {} harap dapat datang ke tempat'.format(item))
+
+
+def nomorarisan(): #akam membuat item dengan memanggil methode release
+    global item
+    time.sleep(3)
+    item = random.randint(1, 10)
+    logging.info('nomor {} adalah pemenang arisan'.format(item))
+    semaphore.release()# dipanggil untuk membebaskan sumber daya 
+
+
+def main():
+    for i in range(1):
+        t1 = threading.Thread(target=pemenangarisan)
+        t2 = threading.Thread(target=nomorarisan)
+
+        t1.start()
+        t2.start()
+
+        t1.join()
+        t2.join()
+
+
+if __name__ == "__main__":
+    main()
