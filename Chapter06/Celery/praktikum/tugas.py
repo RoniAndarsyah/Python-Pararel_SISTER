c1146642d17fa21f18b0c7a72a85b41a6553ba32
DzulJalali <dzuljalali6@gmail.com>
2022-06-28 21:17:21 +0700

    up chapter 7

--- Chapter06/Celery/praktikum/tugas.py
+++ Chapter06/Celery/praktikum/tugas.py
@@ -5,7 +5,8 @@ app = Celery('tugas',broker='amqp://guest@localhost//')
 @app.task
 def longtime_add(x, y):
     print('tugas dimulai')
-    # sleep 5 seconds
+    # sleep 5 detik
     time.sleep(5)
     print('tugas telah selesai')
     return x + y
+    
--- Chapter06/Celery/praktikum/tugas_jalan.py
+++ Chapter06/Celery/praktikum/tugas_jalan.py
@@ -3,10 +3,7 @@ import time
 
 if __name__ == '__main__':
     result = tugas.longtime_add.delay(1,2)
-    # at this time, our task is not finished, so it will return False
     print ('Menunggu Proses Selesai ')
-    print ('Task result: ')
     time.sleep(10)
-    # now the task should be finished and ready method will return True
     print ('Proses telah selesai')
     
